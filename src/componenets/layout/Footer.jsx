23025b3c099e9a1ae4a3842f3a8095bdecd29a6d
red-jen <redaje102105@gmail.com>
2025-07-29 12:41:35 +0100

    footer infos

--- src/componenets/layout/Footer.jsx
+++ src/componenets/layout/Footer.jsx
@@ -108,8 +108,8 @@ const Footer = () => {
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                     </svg>
                   </div>
-                  <a href="tel:+33123456789" className="text-gray-300 hover:text-blue-300 transition-colors">
-                    +33 1 23 45 67 89
+                  <a href="tel:+212661442247" className="text-gray-300 hover:text-blue-300 transition-colors">
+                    +212 661 442 247
                   </a>
                 </div>
                 
@@ -119,8 +119,8 @@ const Footer = () => {
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                     </svg>
                   </div>
-                  <a href="mailto:contact@invision-solutions.fr" className="text-gray-300 hover:text-blue-300 transition-colors">
-                    contact@invision-solutions.fr
+                  <a href="mailto:invision.electro@gmail.com" className="text-gray-300 hover:text-blue-300 transition-colors">
+                    invision.electro@gmail.com
                   </a>
                 </div>
               </div>
